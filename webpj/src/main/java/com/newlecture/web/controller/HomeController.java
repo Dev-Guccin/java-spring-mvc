737fcc62414ea992eb2e3c86af1ba0faf5ecb438
Dev-Guccin <wodhks08@naver.com>
2022-01-26 00:08:19 +0900

    feat: Return String body to client by using 2 ways

--- webpj/src/main/java/com/newlecture/web/controller/HomeController.java
+++ webpj/src/main/java/com/newlecture/web/controller/HomeController.java
@@ -1,10 +1,14 @@
 package com.newlecture.web.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,11 +16,25 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController{ // 컨트롤들을 담는 컨테이너로서 사용한다.
 
 	@RequestMapping("index")
-	public String index() {
+	@ResponseBody
+	public String index(HttpServletResponse response) { // 파라미터를 받겠다고 하면 servelet에서 필요한 파라미터를 넘겨준다.
 		System.out.println("index!!");
-		
-		return "root.index"; // tiles에서 resolving이 가능하므로 문자열을 반환하면 된다.
+		return "BPODY";
 	}
+//	@RequestMapping("index")
+//	public void index(HttpServletResponse response) { // 파라미터를 받겠다고 하면 servelet에서 필요한 파라미터를 넘겨준다.
+//		System.out.println("index!!");
+//		PrintWriter out;
+//		try {
+//			out = response.getWriter();
+//			out.println("WLE");
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+	
+	
 	
 //	@Override
 //	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
