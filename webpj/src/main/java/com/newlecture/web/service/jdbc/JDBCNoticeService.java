13b5e76fa51d7744ab9b85c0e7d3966d134ec1cc
Dev-Guccin <wodhks08@naver.com>
2022-01-25 10:45:01 +0900

    feat: Seperate db init data to pom.xml

--- webpj/src/main/java/com/newlecture/web/service/jdbc/JDBCNoticeService.java
+++ webpj/src/main/java/com/newlecture/web/service/jdbc/JDBCNoticeService.java
@@ -10,15 +10,24 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
 
 public class JDBCNoticeService implements NoticeService{
-	private String url = "jdbc:mysql://localhost:3306/test";
-	private String uid = "root";
-	private String pwd = "4msys!";
-	private String driver = "com.mysql.cj.jdbc.Driver";
+//	private String url = "jdbc:mysql://localhost:3306/test";
+//	private String uid = "root";
+//	private String pwd = "4msys!";
+//	private String driver = "com.mysql.cj.jdbc.Driver";
 	
+	private DataSource dataSource;
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+		
+	}
+
 	public List<Notice> getList(int page, String field, String query) throws ClassNotFoundException, SQLException{
 		
 		int start = 1 + (page-1)*10;     // 1, 11, 21, 31, ..
@@ -27,8 +36,9 @@ public class JDBCNoticeService implements NoticeService{
 		//String sql = "SELECT * FROM NOTICE WHERE "+field+" LIKE ? AND NUM BETWEEN ? AND ?";	
 		String sql = "SELECT * FROM NOTICE WHERE "+field+" LIKE ? LIMIT ?,?";	
 		
-		Class.forName(driver);
-		Connection con = DriverManager.getConnection(url,uid, pwd);
+		//Class.forName(driver);
+		//Connection con = DriverManager.getConnection(url,uid, pwd);
+		Connection con = dataSource.getConnection();
 		PreparedStatement st = con.prepareStatement(sql);
 		st.setString(1, "%"+query+"%");
 		st.setInt(2, start);
@@ -74,8 +84,9 @@ public class JDBCNoticeService implements NoticeService{
 		
 		String sql = "SELECT COUNT(ID) COUNT FROM NOTICE";	
 		
-		Class.forName(driver);
-		Connection con = DriverManager.getConnection(url,uid, pwd);
+		//Class.forName(driver);
+		//Connection con = DriverManager.getConnection(url,uid, pwd);
+		Connection con = dataSource.getConnection();
 		Statement st = con.createStatement();
 		
 		ResultSet rs = st.executeQuery(sql);
@@ -104,8 +115,9 @@ public class JDBCNoticeService implements NoticeService{
 				"    files" + 
 				") VALUES (?,?,?,?)";	
 		
-		Class.forName(driver);
-		Connection con = DriverManager.getConnection(url,uid, pwd);                   
+		//Class.forName(driver);
+		//Connection con = DriverManager.getConnection(url,uid, pwd);
+		Connection con = dataSource.getConnection();
 		//Statement st = con.createStatement();
 		//st.ex....(sql)
 		PreparedStatement st = con.prepareStatement(sql);
@@ -137,8 +149,9 @@ public class JDBCNoticeService implements NoticeService{
 				"    FILES=?" + 
 				"WHERE ID=?";
 		
-		Class.forName(driver);
-		Connection con = DriverManager.getConnection(url,uid, pwd);                   
+		//Class.forName(driver);
+		//Connection con = DriverManager.getConnection(url,uid, pwd);
+		Connection con = dataSource.getConnection();
 		//Statement st = con.createStatement();
 		//st.ex....(sql)
 		PreparedStatement st = con.prepareStatement(sql);
@@ -160,8 +173,9 @@ public class JDBCNoticeService implements NoticeService{
 		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
 		String sql = "DELETE NOTICE WHERE ID=?";
 		
-		Class.forName(driver);
-		Connection con = DriverManager.getConnection(url,uid, pwd);                  
+		//Class.forName(driver);
+		//Connection con = DriverManager.getConnection(url,uid, pwd);
+		Connection con = dataSource.getConnection();
 		//Statement st = con.createStatement();
 		//st.ex....(sql)
 		PreparedStatement st = con.prepareStatement(sql);		
